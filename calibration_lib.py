e3d8453ad3fb114b54b7fa074bc6fa03d793a956
Victor Carneiro Lima <victor.lima@lnls283-vm.abtlus.org.br>
2020-01-07 14:13:26 -0300

    Started petraVII adaptation

--- calibration_lib.py
+++ calibration_lib.py
@@ -20,11 +20,11 @@ val = 0
 
 '''General functions'''
 def GEN_check_loop():
-	LS=ep.caget('BR-RF-DLLRF-01:SL')
+	LS=ep.caget('SR-RF-DLLRF-01:SL')
 	return LS
 
 def GEN_check_RF():
-	PS=ep.caget('RA-RaBO01:RF-LLRFPreAmp:PinSw-Mon')
+	PS=ep.caget('RA-RaBO01:RF-LLRFPreAmp:PinSw-Mon') #NÃO ACHEI
 	return PS
 
 def GEN_write_csv(list,file,setup=None,header=None):
@@ -54,29 +54,30 @@ def GEN_dBmtomVp(dbm_value):
 	return V_value
 
 def GEN_create_LLRF_PVset():
-	BO_LLRF_label=['CAV:AMP','FWDCAV:AMP','REVCAV:AMP','MO:AMP','FWDSSA1:AMP','REVSSA1:AMP','CELL2:AMP','CELL4:AMP','CELL1:AMP','CELL5:AMP','INPRE:AMP','FWDPRE:AMP','REVPRE:AMP','FWDCIRC:AMP','REVCIRC:AMP','SL:REF:AMP','mV:AL:REF','SL:INP:AMP','mV:AMPREF:MIN']
-	PV_header='BR-RF-DLLRF-01:'
-	pv_names=[PV_header]*len(BO_LLRF_label)
-	for i in range(len(BO_LLRF_label)):
-		pv_names[i]=pv_names[i]+BO_LLRF_label[i]
+	SR_LLRF_label=['CAV:AMP','FWDCAV:AMP','REVCAV:AMP','MO:AMP','FWDSSA1:AMP','REVSSA1:AMP','CELL2:AMP','CELL6:AMP','FWDSSA2:AMP','REVSSA2:AMP','INPRE1:AMP','FWDPRE1:AMP','INPRE2:AMP','FWDPRE2:AMP','FWDCIRC:AMP','REVCIRC:AMP','SL:REF:AMP','mV:AL:REF','SL:INP:AMP','mV:AMPREF:MIN']
+	PV_header='SR-RF-DLLRF-01:'
+	pv_names=[PV_header]*len(SR_LLRF_label)
+	for i in range(len(SR_LLRF_label)):
+		pv_names[i]=pv_names[i]+SR_LLRF_label[i]
 	pv_set=[ep.PV(name) for name in pv_names]
 	return pv_set
 
 def GEN_create_CalSys_PVset():
-	PV_header='RA-RaBO01:RF-LLRFCalSys:PwrdBm'
-
-	pv_names=[PV_header]*15
-	for i in range(15):
+	PV_header='RA-RaSIA01:RF-LLRFCalSys:PwrdBm' #CONFERIR
+	
+	pv_names=[PV_header]*16
+	for i in range(16):
 		pv_names[i]=pv_names[i]+str(i+1)+'_CALC'
 	pv_set=[ep.PV(name) for name in pv_names]
 	return pv_set
+
 
 '''Tunning functions'''
 
 def TUN_set_direction(dir):
 	direction=['up','down']
-	PLG1='BR-RF-DLLRF-01:TUNE:PLG1:DIR:S'
-	PLG2='BR-RF-DLLRF-01:TUNE:PLG2:DIR:S'
+	PLG1='SR-RF-DLLRF-01:TUNE:PLG1:DIR:S'
+	PLG2='SR-RF-DLLRF-01:TUNE:PLG2:DIR:S'
 	if(direction[dir]=='up'):
 		ep.caput(PLG1,1)
 		ep.caput(PLG2,1)
@@ -87,8 +88,8 @@ def TUN_set_direction(dir):
 
 def TUN_wait_plunger(dir):
 	direction=['up','down']
-	PLG1='BR-RF-DLLRF-01:PLG1:MANUAL:'
-	PLG2='BR-RF-DLLRF-01:PLG2:MANUAL:'
+	PLG1='SR-RF-DLLRF-01:PLG1:MANUAL:'
+	PLG2='SR-RF-DLLRF-01:PLG2:MANUAL:'
 
 	if(direction[dir]=='up'):
 		if(ep.caget(PLG1+'UP') or ep.caget(PLG2+'UP')):
@@ -103,7 +104,7 @@ def TUN_wait_plunger(dir):
 
 def TUN_move_plunger(dir,pulses):
 	speed='2 kHz'
-	PV_header='BR-RF-DLLRF-01:TUNE:'
+	PV_header='SR-RF-DLLRF-01:TUNE:'
 	direction=['up','down']
 
 	ep.caput(PV_header+'PULSE:FREQ:S',speed)
@@ -120,14 +121,14 @@ def TUN_move_plunger(dir,pulses):
 		time.sleep(1)
 	return 0
 
-def TUN_find_offset(ref_threshold=27):
+def TUN_find_offset(ref_threshold=13):
 	iterations=0
 	pulses=1500
-	PV_header='BR-RF-DLLRF-01:'
+	PV_header='SR-RF-DLLRF-01:'
 	global direction_sel
 	it_limit=11
-	fwd_power=ep.caget('RA-RaBO01:RF-LLRFCalSys:PwrdBm14-Mon')
-	rev_power=ep.caget('RA-RaBO01:RF-LLRFCalSys:PwrdBm15-Mon')
+	fwd_power=ep.caget('RA-RaSIA01:RF-LLRFCalSys:PwrdBm15-Mon')
+	rev_power=ep.caget('RA-RaSIA01:RF-LLRFCalSys:PwrdBm16-Mon')
 	diff=fwd_power-rev_power
 	print('Ref ratio '+str(diff))
 	if(isnan(diff)):
@@ -143,8 +144,8 @@ def TUN_find_offset(ref_threshold=27):
 	while(diff<ref_threshold and iterations<=it_limit):
 		TUN_move_plunger(direction_sel,pulses)
 		diff_old=diff
-		fwd_power=ep.caget('RA-RaBO01:RF-LLRFCalSys:PwrdBm14-Mon')
-		rev_power=ep.caget('RA-RaBO01:RF-LLRFCalSys:PwrdBm15-Mon')	
+		fwd_power=ep.caget('RA-RaSIA01:RF-LLRFCalSys:PwrdBm15-Mon')
+		rev_power=ep.caget('RA-RaSIA01:RF-LLRFCalSys:PwrdBm16-Mon')
 		diff=fwd_power-rev_power
 		print('Ref ratio '+str(diff))
 		if(isnan(diff)):
@@ -171,7 +172,7 @@ def clean_val():
 def PWR_read_CalSys(pv_set,var,inf_flag='inf',ofs_flag='noofs',avg='noavg'):
 	if(var.startswith('RFIn')):
 		RFIn=int(var[4:])
-		if(RFIn<1 or RFIn>15):
+		if(RFIn<1 or RFIn>16):
 			raise ValueError('Channel '+str(RFIn)+' does not exist')
 	else:
 		raise ValueError('Channel '+var+' does not exist')
@@ -183,7 +184,7 @@ def PWR_read_CalSys(pv_set,var,inf_flag='inf',ofs_flag='noofs',avg='noavg'):
 		if (pwr<-42 and inf_flag!='noinf'):
 			return -np.inf
 		if(ofs_flag=='ofs'):
-			ofs=ep.caget('RA-RaBO01:RF-LLRFCalSys:OFSdB'+str(RFIn)+'-Mon')
+			ofs=ep.caget('RA-RaSIA01:RF-LLRFCalSys:OFSdB'+str(RFIn)+'-Mon')
 			return pwr+ofs
 		return pwr
 	else:
@@ -208,7 +209,7 @@ def PWR_read_CalSys(pv_set,var,inf_flag='inf',ofs_flag='noofs',avg='noavg'):
 		if(avg<-42 and inf_flag!='noinf'):
 			return -np.inf
 		if(ofs_flag=='ofs'):
-			ofs=ep.caget('RA-RaBO01:RF-LLRFCalSys:OFSdB'+str(RFIn)+'-Mon')
+			ofs=ep.caget('RA-RaSIA01:RF-LLRFCalSys:OFSdB'+str(RFIn)+'-Mon')
 			return avg+ofs
 		return avg
 
@@ -216,14 +217,14 @@ def PWR_read_LLRF(pv_set,var,avg='noavg'):
 
 	if(var.startswith('RFIn')):
 		RFIn=int(var[4:])
-		if(RFIn<1 or RFIn>15):
+		if(RFIn<1 or RFIn>16):
 			raise ValueError('Channel '+str(RFIn)+' does not exist')
 	elif(var=='AmpRef'):
-		RFIn=16
-	elif(var=='AmpSP'):
 		RFIn=17
+	elif(var=='AmpSP'):
+		RFIn=18
 	elif(var=='tst'):
-		RFIn=19
+		RFIn=20
 	else:
 		raise ValueError('Channel '+var+' does not exist')
 
@@ -253,21 +254,21 @@ def PWR_read_LLRF(pv_set,var,avg='noavg'):
 		return avg
 
 def PWR_read_LLRF_coeff(var,type):
-	BO_LLRF_label=['CAV','FWDCAV','REVCAV','MO','FWDSSA1','REVSSA1','CELL2','CELL4','CELL1','CELL5','INPRE','FWDPRE','REVPRE','FWDCIRC','REVCIRC']
+	SR_LLRF_label=['CAV','FWDCAV','REVCAV','MO','FWDSSA1','REVSSA1','CELL2','CELL6','FWDSSA2','REVSSA2','INPRE1','FWDPRE1','INPRE2','FWDPRE2','FWDCIRC','REVCIRC']
 	coeff=np.zeros(5)
 	if(var.startswith('RFIn')):
 		RFIn=int(var[4:])
-		if(RFIn<1 or RFIn>15):
+		if(RFIn<1 or RFIn>16):
 			raise ValueError('Channel '+str(RFIn)+' does not exist')
 	else:
 		raise ValueError('Channel '+var+' does not exist')
-	PV_header='BR-RF-DLLRF-01:'
+	PV_header='SR-RF-DLLRF-01:'
 	if(type=='Raw-U'):
-		PV_name=PV_header+BO_LLRF_label[RFIn-1]+':Const:Raw-U:'
+		PV_name=PV_header+SR_LLRF_label[RFIn-1]+':Const:Raw-U:'
 	elif(type=='U-Raw'):
-		PV_name=PV_header+BO_LLRF_label[RFIn-1]+':Const:U-Raw:'
+		PV_name=PV_header+SR_LLRF_label[RFIn-1]+':Const:U-Raw:'
 	elif(type=='OLG'):
-		PV_name=PV_header+'OLG:'+BO_LLRF_label[RFIn-1]+':Const:'
+		PV_name=PV_header+'OLG:'+SR_LLRF_label[RFIn-1]+':Const:'
 	else:
 		raise ValueError('Type does not exist')
 	coeff[0]=ep.caget(PV_name+'C4')
@@ -278,103 +279,35 @@ def PWR_read_LLRF_coeff(var,type):
 	return coeff
 
 def PWR_set_LLRF_coeff(coef):
-	if(not coef.shape==(6,21)):
+	if(not coef.shape==(6,24)):
 		raise ValueError ('Wrong coefficient array size')
-	PV_header='BR-RF-DLLRF-01:'
-	BO_LLRF_label=['CAV','FWDCAV','FWDSSA1','CAV','FWDCAV','REVCAV','MO','FWDSSA1','REVSSA1','CELL2','CELL4','CELL1','CELL5','INPRE','FWDPRE','REVPRE','FWDCIRC','REVCIRC','CAV','FWDCAV','FWDSSA1']
-	LLRF_Ramp_label=['CELL3','CELL2','CELL4','FWDCAV','FWDSSA1','REVSSA1','REVCAV']
-	ramp_index=[3,9,10,4,7,8,5]
+	PV_header='SR-RF-DLLRF-01:'
+	SR_LLRF_label=['CAV','FWDCAV','FWDSSA1','FWDSSA2','CAV','FWDCAV','REVCAV','MO','FWDSSA1','REVSSA1','CELL2','CELL6','FWDSSA2','REVSSA2','INPRE1','FWDPRE1','INPRE2','FWDPRE2','FWDCIRC','REVCIRC','CAV','FWDCAV','FWDSSA1','FWDSSA2']
 	j=0
-	for i in range(0,21):
-		if i<3:
-			logging.info('Setting '+BO_LLRF_label[i]+' OLG Coefficients')
-			print('Setting '+BO_LLRF_label[i]+' OLG Coefficients')
-			PV_name=PV_header+'OLG:'+BO_LLRF_label[i]+':Const:'
+	for i in range(0,24):
+		if i<4:
+			logging.info('Setting '+SR_LLRF_label[i]+' OLG Coefficients')
+			print('Setting '+SR_LLRF_label[i]+' OLG Coefficients')
+			PV_name=PV_header+'OLG:'+SR_LLRF_label[i]+':Const:'
 			ep.caput(PV_name+'C4:S',coef[0,i])
 			ep.caput(PV_name+'C3:S',coef[1,i])
 			ep.caput(PV_name+'C2:S',coef[2,i])
 			ep.caput(PV_name+'C1:S',coef[3,i])
 			ep.caput(PV_name+'C0:S',coef[4,i])
-		elif i<18:
-			logging.info('Setting '+BO_LLRF_label[i]+' RAW-U Coefficients')
-			print('Setting '+BO_LLRF_label[i]+' RAW-U Coefficients')
-			PV_name=PV_header+BO_LLRF_label[i]+':Const:Raw-U:'
+		elif i<20:
+			logging.info('Setting '+SR_LLRF_label[i]+' RAW-U Coefficients')
+			print('Setting '+SR_LLRF_label[i]+' RAW-U Coefficients')
+			PV_name=PV_header+SR_LLRF_label[i]+':Const:Raw-U:'
 			ep.caput(PV_name+'C4:S',coef[0,i])
 			ep.caput(PV_name+'C3:S',coef[1,i])
 			ep.caput(PV_name+'C2:S',coef[2,i])
 			ep.caput(PV_name+'C1:S',coef[3,i])
 			ep.caput(PV_name+'C0:S',coef[4,i])
-			ep.caput(PV_header+BO_LLRF_label[i]+':Const:OFS:S',coef[5,i])
-			if(i in ramp_index):
-				if(i==3):
-					logging.info('Setting '+BO_LLRF_label[i]+' RAW-U TOP Coefficients')
-					print('Setting '+BO_LLRF_label[i]+' RAW-U TOP Coefficients')
-					PV_name=PV_header+'TOP:CELL3:Const:Raw-U:'
-					ep.caput(PV_name+'C4:S',coef[0,i])
-					ep.caput(PV_name+'C3:S',coef[1,i])
-					ep.caput(PV_name+'C2:S',coef[2,i])
-					ep.caput(PV_name+'C1:S',coef[3,i])
-					ep.caput(PV_name+'C0:S',coef[4,i])
-					ep.caput(PV_header+'TOP:CELL3:Const:OFS:S',coef[5,i])
-
-
-					logging.info('Setting '+BO_LLRF_label[i]+' RAW-U BOT Coefficients')
-					print('Setting '+BO_LLRF_label[i]+' RAW-U BOT Coefficients')
-					PV_name=PV_header+'BOT:CELL3:Const:Raw-U:'
-					ep.caput(PV_name+'C4:S',coef[0,i])
-					ep.caput(PV_name+'C3:S',coef[1,i])
-					ep.caput(PV_name+'C2:S',coef[2,i])
-					ep.caput(PV_name+'C1:S',coef[3,i])
-					ep.caput(PV_name+'C0:S',coef[4,i])
-					ep.caput(PV_header+'BOT:CELL3:Const:OFS:S',coef[5,i])
-					'''
-					logging.info('Setting REF RAW-U TOP Coefficients')
-					print('Setting REF RAW-U TOP Coefficients')
-					PV_name=PV_header+'TOP:REF:Const:Raw-U:'
-					ep.caput(PV_name+'C4:S',coef[0,i])
-					ep.caput(PV_name+'C3:S',coef[1,i])
-					ep.caput(PV_name+'C2:S',coef[2,i])
-					ep.caput(PV_name+'C1:S',coef[3,i])
-					ep.caput(PV_name+'C0:S',coef[4,i])
-					ep.caput(PV_header+'TOP:REF:Const:OFS:S',coef[5,i])
-
-					logging.info('Setting REF RAW-U BOT Coefficients')
-					print('Setting REF RAW-U BOT Coefficients')
-					PV_name=PV_header+'BOT:REF:Const:Raw-U:'
-					ep.caput(PV_name+'C4:S',coef[0,i])
-					ep.caput(PV_name+'C3:S',coef[1,i])
-					ep.caput(PV_name+'C2:S',coef[2,i])
-					ep.caput(PV_name+'C1:S',coef[3,i])
-					ep.caput(PV_name+'C0:S',coef[4,i])
-					ep.caput(PV_header+'BOT:REF:Const:OFS:S',coef[5,i])
-					'''
-
-				else:
-					logging.info('Setting '+BO_LLRF_label[i]+' RAW-U TOP Coefficients')
-					print('Setting '+BO_LLRF_label[i]+' RAW-U TOP Coefficients')
-					PV_name=PV_header+'TOP:'+BO_LLRF_label[i]+':Const:Raw-U:'
-					ep.caput(PV_name+'C4:S',coef[0,i])
-					ep.caput(PV_name+'C3:S',coef[1,i])
-					ep.caput(PV_name+'C2:S',coef[2,i])
-					ep.caput(PV_name+'C1:S',coef[3,i])
-					ep.caput(PV_name+'C0:S',coef[4,i])
-					ep.caput(PV_header+'TOP:'+BO_LLRF_label[i]+':Const:OFS:S',coef[5,i])
-
-
-					logging.info('Setting '+BO_LLRF_label[i]+' RAW-U BOT Coefficients')
-					print('Setting '+BO_LLRF_label[i]+' RAW-U BOT Coefficients')
-					PV_name=PV_header+'BOT:'+BO_LLRF_label[i]+':Const:Raw-U:'
-					ep.caput(PV_name+'C4:S',coef[0,i])
-					ep.caput(PV_name+'C3:S',coef[1,i])
-					ep.caput(PV_name+'C2:S',coef[2,i])
-					ep.caput(PV_name+'C1:S',coef[3,i])
-					ep.caput(PV_name+'C0:S',coef[4,i])
-					ep.caput(PV_header+'BOT:'+BO_LLRF_label[i]+':Const:OFS:S',coef[5,i])
-
+			ep.caput(PV_header+SR_LLRF_label[i]+':Const:OFS:S',coef[5,i])
 		else:
-			logging.info('Setting '+BO_LLRF_label[i]+' U-RAW Coefficients')
-			print('Setting '+BO_LLRF_label[i]+' U-RAW Coefficients')
-			PV_name=PV_header+BO_LLRF_label[i]+':Const:U-Raw:'
+			logging.info('Setting '+SR_LLRF_label[i]+' U-RAW Coefficients')
+			print('Setting '+SR_LLRF_label[i]+' U-RAW Coefficients')
+			PV_name=PV_header+SR_LLRF_label[i]+':Const:U-Raw:'
 			ep.caput(PV_name+'C4:S',coef[0,i])
 			ep.caput(PV_name+'C3:S',coef[1,i])
 			ep.caput(PV_name+'C2:S',coef[2,i])
@@ -382,20 +315,20 @@ def PWR_set_LLRF_coeff(coef):
 			ep.caput(PV_name+'C0:S',coef[4,i])
 
 def PWR_read_LLRF_ofs(var):
-	BO_LLRF_label=['CAV','FWDCAV','REVCAV','MO','FWDSSA1','REVSSA1','CELL2','CELL4','CELL1','CELL5','INPRE','FWDPRE','REVPRE','FWDCIRC','REVCIRC']
+	SR_LLRF_label=['CAV','FWDCAV','REVCAV','MO','FWDSSA1','REVSSA1','CELL2','CELL6','FWDSSA2','REVSSA2','INPRE1','FWDPRE1','INPRE2','FWDPRE2','FWDCIRC','REVCIRC']
 	if(var.startswith('RFIn')):
 		RFIn=int(var[4:])
-		if(RFIn<1 or RFIn>15):
+		if(RFIn<1 or RFIn>16):
 			raise ValueError('Channel '+str(RFIn)+' does not exist')
 	else:
 		raise ValueError('Channel '+var+' does not exist')
 	PV_header='BR-RF-DLLRF-01:'
-	PV_name=PV_header+BO_LLRF_label[RFIn-1]+':Const:'
+	PV_name=PV_header+SR_LLRF_label[RFIn-1]+':Const:'
 	ofs=ep.caget(PV_name+'OFS')
 	return ofs
 
 def PWR_set_power_mv(lvl,incrate='1.0'):
-	PV_header='BR-RF-DLLRF-01:'
+	PV_header='SR-RF-DLLRF-01:'
 	values=['0.01','0.03','0.1','0.25','0.5','1.0','2.0','Immediately']
 	if incrate in values:
 		ep.caput(PV_header+'AMPREF:INCRATE:S',incrate)
@@ -407,22 +340,21 @@ def PWR_set_power_mv(lvl,incrate='1.0'):
 		raise ValueError('mV value above maximum')
 	return 0
 
-
 '''Phase functions'''
 
 def PHS_read(var):
 	if(var=='PhsIn'):
-		phs=ep.caget('BR-RF-DLLRF-01:SL:INP:PHS')
+		phs=ep.caget('SR-RF-DLLRF-01:SL:INP:PHS')
 	elif(var=='PhsSP'):
-		phs=ep.caget('BR-RF-DLLRF-01:PL:REF')
+		phs=ep.caget('SR-RF-DLLRF-01:PL:REF')
 	else:
 		raise ValueError('Invalid parameter: '+var)
 
 def PHS_set_phase(lvl):
 
-	ep.caput('BR-RF-DLLRF-01:PHSREF:INCRATE:S','2.0')
+	ep.caput('SR-RF-DLLRF-01:PHSREF:INCRATE:S','2.0')
 	if(lvl>=-180 and lvl<=180):
-		ep.caput('BR-RF-DLLRF-01:PL:REF:S',lvl)
+		ep.caput('SR-RF-DLLRF-01:PL:REF:S',lvl)
 		return 0
 	else:
 		raise ValueError('Phase out of range')
